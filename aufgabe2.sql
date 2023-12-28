f951aae039590bc9c0dbce58ad0548777ed16e1b
Colin Bankart <baco1012@h-ka.de>
2023-12-28 15:48:54 +0100

    ordnung fuer aufgabe2

--- aufgabe2.sql
+++ aufgabe2.sql
@@ -1,15 +1,14 @@
+--2.1--
 select auftrnr, datum from auftrag a 
 	where kundnr in 
 		(select k.nr from kunde k where k.name = 'Fahrrad Shop')
 ;
-
-select * from kunde k;
-
+--2.2--
 select auftrnr, datum from auftrag a 
 	where kundnr = some
 		(select k.nr from kunde k where k.name = 'Fahrrad Shop')
 	;
-
+--2.3--
 select auftrnr, datum from auftrag a 
 	where exists 
 		(select k.nr from kunde k 
@@ -17,32 +16,32 @@ select auftrnr, datum from auftrag a
 		and a.kundnr = k.nr
 		)
 ;
-
+--2.4--
 select kundnr, count(auftrnr) as anzahl, min(datum) as von, max(datum) as bis 
 	from auftrag a 
 	group by kundnr 
 	order by kundnr
 ;
-
+--2.5--
 select kundnr, count(auftrnr) as anzahl, min(datum) as von, max(datum) as bis 
 	from auftrag a 
 	group by kundnr 
 	having count(auftrnr) = 1
 	order by kundnr
 ;
-	
+--2.6--	
 select k.nr, k.name, a.auftrnr 
 	from kunde k 
 	left join auftrag a on a.kundnr  = k.nr 
 	order by k.nr
 ;
-
+--2.7--
 select k.nr, k.name, a.auftrnr, p.name from kunde k 
 	left join auftrag a on a.kundnr  = k.nr 
 	join personal p on a.persnr = p.persnr
 	order by k.nr
 ;
-
+--2.8--
 select k.name, sum(a2.gesamtpreis) from kunde k 
 	join auftrag a on k.nr = a.kundnr
 	join auftragsposten a2 on a.auftrnr = a2.auftrnr
@@ -50,7 +49,7 @@ select k.name, sum(a2.gesamtpreis) from kunde k
 	order by sum(a2.gesamtpreis) desc
 	fetch first 1 row only
 	;
-
+--2.9--
 with kundenSumme as (
 	select k.name, sum(a2.gesamtpreis) from kunde k 
 	join auftrag a on k.nr = a.kundnr
@@ -58,10 +57,10 @@ with kundenSumme as (
 	group by k.nr 
 )
 select * from kundenSumme 
-order by sum desc
-fetch first row only
+	order by sum desc
+	fetch first row only
 ;
-
+--2.10--
 create view kundenUmsatz as 
 	with kundenSumme as (
 	select k.name, sum(a2.gesamtpreis) from kunde k 
@@ -69,10 +68,9 @@ create view kundenUmsatz as
 	join auftragsposten a2 on a.auftrnr = a2.auftrnr
 	group by k.nr 
 )
-select * from kundenSumme 
-order by sum desc
+	select * from kundenSumme 
+	order by sum desc
 ;
-
 select * from kundenumsatz;
-
+--2.11--
 drop view if exists kundenUmsatz;
