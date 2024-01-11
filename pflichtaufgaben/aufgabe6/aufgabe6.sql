0dfd7bc56c089608562e52495e9394c89ebb78ba
Colin Bankart <baco1012@h-ka.de>
2024-01-11 15:37:18 +0100

    aufgabe6 ist jetzt .sql fertig. 2.6.2 mNormalisierung muss noch gemacht werden.

--- pflichtaufgaben/aufgabe6/aufgabe6.sql
+++ pflichtaufgaben/aufgabe6/aufgabe6.sql
@@ -1,43 +1,21 @@
 --aufgabe6--
+begin;
+
 --Studiengang--
 CREATE TABLE Studiengang (
     Kuerzel VARCHAR(255) PRIMARY KEY,
     Name VARCHAR(255) NOT NULL
 );
 
---Dozent--
-CREATE TABLE Dozent (
-    ID SERIAL PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL,
-    Vorname VARCHAR(255) NOT NULL,
-    Grad VARCHAR(255),
-    Email VARCHAR(255) NOT null
-    foreign key (ID) references Veranstaltung(ID)
-);
-
---Erweiterung von Dozent um Daten welche auf Professoren und Mitarbeiter beschraenkt sind--
-CREATE TABLE Professor_Mitarbeiter (
-    ID INT PRIMARY KEY,
-    Buero VARCHAR(255),
-    Sprechzeiten VARCHAR(255),
-    FOREIGN KEY (ID) REFERENCES Dozent(ID)
-);
 --gebaeude--
 CREATE TABLE Gebaeude (
     Nummer SERIAL PRIMARY KEY,
     Name VARCHAR(255) NOT NULL
 );
---raum, per gebaeudenummer mit gebaeude verknuepft--
-CREATE TABLE Raum (
-    Nummer SERIAL PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL,
-    GebaeudeNummer INT,
-    FOREIGN KEY (GebaeudeNummer) REFERENCES Gebaeude(Nummer)
-);
+
 --veranstaltung, Typ als attribut angegeben, da sonst nirgends verwendet--
 CREATE TABLE Veranstaltung (
-    ID INTEGER PRIMARY KEY, --hinzugefuegt--
-	Name VARCHAR(255),
+	Name VARCHAR(255) primary key,
     Typ VARCHAR(255) CHECK(Typ IN ('Labor', 'Übung', 'Vorlesung')),
     Wochentag INTEGER,
     Startzeit TIME,
@@ -48,6 +26,31 @@ CREATE TABLE Veranstaltung (
     FOREIGN KEY (StudiengangKuerzel) REFERENCES Studiengang(Kuerzel)
 );
 
+--Dozent--
+CREATE TABLE Dozent (
+    ID SERIAL PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Vorname VARCHAR(255) NOT NULL,
+    Grad VARCHAR(255),
+    Email VARCHAR(255) NOT null,
+    VeranstaltungName varchar(255) REFERENCES Veranstaltung(Name)
+);
+--Erweiterung von Dozent um Daten welche auf Professoren und Mitarbeiter beschraenkt sind--
+CREATE TABLE Professor_Mitarbeiter (
+    ID INT PRIMARY KEY,
+    Buero VARCHAR(255),
+    Sprechzeiten VARCHAR(255),
+    DozentID Integer REFERENCES Dozent(ID)
+);
+--raum, per gebaeudenummer mit gebaeude verknuepft--
+CREATE TABLE Raum (
+    Nummer SERIAL PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    GebaeudeNummer Integer,
+    VeranstaltungName varchar(255),
+    FOREIGN KEY (GebaeudeNummer) REFERENCES Gebaeude(Nummer),
+    FOREIGN KEY (VeranstaltungName) REFERENCES Veranstaltung(Name)
+);
 
 --stundenplan, per foreign key mir Studiengang verknuepft--
 CREATE TABLE Stundenplan (
@@ -64,3 +67,14 @@ CREATE TABLE Stundenplan_Veranstaltung (
     FOREIGN KEY (StundenplanID) REFERENCES Stundenplan(ID),
     FOREIGN KEY (VeranstaltungName) REFERENCES Veranstaltung(Name)
 );
+
+drop table if exists studiengang cascade;
+drop table if exists dozent cascade;
+drop table if exists professor_Mitarbeiter cascade;
+drop table if exists gebaeude cascade;
+drop table if exists raum cascade;
+drop table if exists stundenplan_Veranstaltung cascade;
+drop table if exists veranstaltung cascade;
+drop table if exists stundenplan cascade;
+
+commit;
